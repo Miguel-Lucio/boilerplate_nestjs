24ab02d792538ae11619bf180b78ab7439ceb184
Miguel Lúcio <miguelluciosilva@gmail.com>
2025-06-05 19:15:56 -0300

    feat: criação middleware de erro

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,17 @@ import "reflect-metadata";
 import "express-async-errors";
 import express, { Application, json } from "express";
 import cors from "cors";
+import { handleAppError } from "./middlewares/handleAppError.middleware";
 
 const app: Application = express();
 app.use(json());
 app.use(
   cors({
     origin: ["http://localhost:5173"],
+    credentials: true,
   })
 );
+
+app.use(handleAppError);
 
 export default app;
