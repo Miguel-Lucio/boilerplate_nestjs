c2cc01b091f53f4bec3beac78578f5a671136b17
Miguel Lúcio <miguelluciosilva@gmail.com>
2025-06-05 21:56:27 -0300

    feat; finalização das rotas de empresas

--- src/routes/company.route.ts
+++ src/routes/company.route.ts
@@ -1,8 +1,12 @@
 import { Request, Response, Router } from "express";
 import { companyController } from "../controllers";
 import { validateBody } from "../middlewares/validateBody.middleware";
-import { companyCreateSchema } from "../schemas/company.schema";
+import {
+  companyCreateSchema,
+  companyUpdateSchema,
+} from "../schemas/company.schema";
 import { validCnpj } from "../middlewares/validCnpj.middleware";
+import { validId } from "../middlewares/validId.middleware";
 
 export const companyRouter = Router();
 
@@ -14,3 +18,23 @@ companyRouter.post(
     companyController.create(req, res);
   }
 );
+
+companyRouter.get("/", (req: Request, res: Response) => {
+  companyController.read(req, res);
+});
+
+companyRouter.use("/:id", validId);
+companyRouter.get("/:id", (req: Request, res: Response) => {
+  companyController.readOne(req, res);
+});
+companyRouter.patch(
+  "/:id",
+  validateBody(companyUpdateSchema),
+  validCnpj,
+  (req: Request, res: Response) => {
+    companyController.update(req, res);
+  }
+);
+companyRouter.delete("/:id", (req: Request, res: Response) => {
+  companyController.remove(req, res);
+});
